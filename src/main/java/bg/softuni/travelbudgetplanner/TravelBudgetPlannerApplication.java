7057d8289912157c4a7c75887a1f8762aa7901f7
Boyan Antonov <boyan.antonov02@gmail.com>
2024-06-26 15:55:30 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/travelbudgetplanner/TravelBudgetPlannerApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.travelbudgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBudgetPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBudgetPlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/travelbudgetplanner/TravelBudgetPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.travelbudgetplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBudgetPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
